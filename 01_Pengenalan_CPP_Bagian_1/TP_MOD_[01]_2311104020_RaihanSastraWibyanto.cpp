c6f4781469bfa29763bc628ff2ac3acce16df5d9
DavidCatch <raihanwibyanto@gmail.com>
2024-09-28 15:36:52 +0700

    Update

--- 01_Pengenalan_CPP_Bagian_1/TP_MOD_[01]_2311104020_RaihanSastraWibyanto.cpp
+++ 01_Pengenalan_CPP_Bagian_1/TP_MOD_[01]_2311104020_RaihanSastraWibyanto.cpp
@@ -84,7 +84,7 @@ int main(){
  */
 
 /* Percabangan if-else */
-/**
+/** 
 #include <iostream>
 using namespace std;
 
@@ -98,7 +98,7 @@ int main(){
     }
     return 0;
 }
- */
+*/
 
 /* Perulangan for-to-do */
 /**
@@ -119,7 +119,7 @@ int main (){
 */
 
 /* Perulangan while-do */
-/** 
+
 #include <iostream>
 using namespace std;
 
@@ -138,99 +138,6 @@ int main (){
         bilangan++;
     }
     cout << "Jumlah bilangan genap: " << jumlah << endl;
-    return 0;
-}
-*/
-
-// Latihan
-/* No 1 */
-/**
-#include <iostream>
-#include <stdio.h>
-using namespace std;
-
-int main ()
-{
-    float bil1,bil2;
-    cout << "Masukkan Bilangan1: ";
-    cin >> bil1;
-    cout <<endl;
-    cout << "Masukkan Bilangan2: ";
-    cin >> bil2;
-    cout <<endl;
-
-    cout << "Hasil Penjumlahan adalah " << bil1 + bil2 <<endl;
-    cout << "Hasil Pengurangan adalah " << bil1 - bil2 <<endl;
-    cout << "Hasil Perkalian adalah " << bil1 * bil2 <<endl;
-    cout << "Hasil Pembagian adalah " << bil1 / bil2 ;
-
-    return 0;
-}
- */
-
-/* No 2 */
-/**
-#include <iostream>
-using namespace std;
-
-int main() {
-    // Array untuk menyimpan huruf angka
-    string satuan [] = {"Nol","Satu","Dua","Tiga","Empat","Lima",
-    "Enam","Tujuh","Delapan","Sembilan"};
-    string belasan [] = {"Sepuluh","Sebelas","Dua Belas","Tiga Belas",
-    "Empat Belas", "Lima Belas", "Enam Belas", "Tujuh Belas", "Delapan Belas", "Sembilan Belas"};
-    string puluhan [] = {" "," ","Dua Puluh","Tiga Puluh", "Empat Puluh", "Lima Puluh", "Enam Puluh",
-    "Tujuh Puluh", "Delapan Puluh", "Sembilan Puluh"};
-
-    int angka; // membuat variabel angka
-
-    // Input
-    cout << "Ket : Masukkan Angka (0-100) \n";
-    cout << "----------------------------------\n";
-    cin >> angka;
-    
-    // membuat validasi angka dengan percabangan if-else
-    if (angka < 0 || angka > 100){
-        cout << "Angka yang anda masukkan tidak ada!!!" <<endl;
-    }else if (angka == 100){
-        cout << "Seratus" <<endl;
-    }else if (angka >= 0 && angka <= 9){
-        cout << satuan[angka] <<endl;
-    }else if (angka >= 10 && angka <= 19){
-        cout << belasan[angka - 10] <<endl;
-    }else{
-        int puluh = angka /10 ;
-        int sisaAngka = angka % 10;
-        cout << puluhan[puluh];
-        if(sisaAngka != 0){
-            cout << " " << satuan[sisaAngka];
-        }
-        cout <<endl;
-    }
-    return 0;
-}
- */
-
-/* No 3 */
-
-#include <iostream>
-using namespace std;
- 
-int main()
-{
-    int n = 3;
- 
-    for (int i = 1; i <= n + 1; i++) {
-        
-        for (int j = 1; j < i; j++)
-            cout << "  ";
-        for (int j = n - i + 1; j >= 1; j--) // Mencetak angka turun
-            cout << j << " ";
-        cout << "* ";
-        for(int j = 1; j <= n - i + 1; j++) // Mencetak angka naik 
-        	cout << j << " ";
-        cout << endl;
-    }
     return 0;
 }
 
